39b9a5f1e054ff0ad0a0bd8e1525576c8b4a3861
Ryan <snorekr@gmail.com>
2021-12-08 07:14:51 -0500

    router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    res.json("check")
+})
+
+router.get("/:id", (req, res, next) => {
+    res.json("check")
+})
+
+router.post("/", (req, res, next) => {
+    res.json("check")
+})
+
+module.exports = router;
