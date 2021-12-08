2bb1cc61c65eb413db4245714ff7ec50c3904460
Ryan <snorekr@gmail.com>
2021-12-08 07:09:09 -0500

    server, migrations

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,31 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
-};
+exports.up = function(knex) {
+  return knex.schema.createTable("cars", table => {
+    // creates a column for primary id. default label is "id"
+    table.increments()
+    
+    table.string("vin", 20).notNullable().unique()
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+    table.string("make", 100).notNullable()
+
+    table.string("model", 100).notNullable()
+
+    table.integer("milage").unsigned().notNullable()
+
+    table.string("title", 100)
+    // .defaultTo("clean")
+
+    table.string("transmission", 100)
+  })
+};
+// To make changes you either delete the DB and re run migrations
+// Or migrate the DB down and then back up (may need to do go down many times)
+// scripts: 
+// "rollback": knex migrate:rollback
+// "up": knex migrate:up
+// "down": knex migrate:down
+
+// If there is more than 1 table you must drop them in the order they were created
+// The last thing done by the up function is the first thing done by the down function
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars")
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,10 @@
-// STRETCH
+const data = [
+    {
+        stuff: ""
+    }
+]
+
+exports.seed = async function(knex) {
+    await knex("data").truncate();
+    await knex("data").insert(data)
+}
